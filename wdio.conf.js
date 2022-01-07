bb12e887fcbf34485be2a75c1798b65952621cfe
Shekar Juttu <shekarchandra123@gmail.com>
2022-01-07 20:49:05 +0530

    qwqwq

--- wdio.conf.js
+++ wdio.conf.js
@@ -65,7 +65,7 @@ exports.config = {
         'goog:chromeOptions': {
             // to run chrome headless the following flags are required
             // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-             args: ['--no-sandbox'],
+             args: ['--no-sandbox', '--disable-dev-shm-usage'],
             }
             //
             // Parameter to ignore some or all default flags
